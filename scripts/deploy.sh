789de87dc3606fc6a86898318ee7990cbab82e22
Jeff Mulligan <jbmull@gmail.com>
2023-01-04 00:27:23 -0800

    main:  first deployment

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+	echo "Usage: ./scripts/deploy.sh <presentation_dir>"
+	exit 1
+fi
+
+presentation_dir=$1
+
+if [[ ! -e $presentation_dir ]]; then
+	echo "Presentation dir $presentation_dir does not exist"
+	exit 1
+fi
+
+target_dir=docs/$presentation_dir
+if [[ ! -e $target_dir ]]; then
+	echo "Creating target dir $target_dir"
+	mkdir $target_dir
+fi
+
+cp $presentation_dir/*.htm $target_dir
+
