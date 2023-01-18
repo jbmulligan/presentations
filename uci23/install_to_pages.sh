ee2426ba4baf61a53facd6103afd978f0f0f85d4
Jeff Mulligan <jbmull@gmail.com>
2023-01-18 08:42:12 -0800

    more images

--- uci23/install_to_pages.sh
+++ uci23/install_to_pages.sh
@@ -4,5 +4,5 @@
 # BUG - should use symlinks instead of copies
 
 cp -p ./*.htm ../docs/uci23
-cp -pR ../datalit_slides/* ../docs/datalit_slides
+cp -pR ../datalit_slides/images/* ../docs/datalit_slides/images
 
